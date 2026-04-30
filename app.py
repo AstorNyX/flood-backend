04aab5a6cd64fc3fd5eaa47d643793e20c7f7c7d
AstorNyX <techmate4132@gmail.com>
2026-05-01 01:02:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ from fastapi import FastAPI
 import pickle
 import numpy as np
 import requests
-import os
 
 app = FastAPI()
 
 # ===== LOAD MODEL =====
 model = pickle.load(open("model.pkl", "rb"))
-# ===== TELEGRAM CONFIG =====
-TOKEN = os.getenv("8573374564:AAFmOnDbMd1r2DVJbKtIs03gnj-b6yY6D98")     # set in Render
-CHAT_ID = os.getenv("-1003989233809")     # set in Render
 
-last_state = -1   # prevents spam
+# ===== TELEGRAM CONFIG (HARDCODED FOR NOW) =====
+TOKEN = "8573374564:AAFmOnDbMd1r2DVJbKtIs03gnj-b6yY6D98"
+CHAT_ID = "-1003989233809"
 
+# ===== STORE LAST DATA =====
+last_data = {
+    "water": 0,
+    "rain": 0,
+    "state": 0
+}
+
+# ===== TELEGRAM FUNCTION =====
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     data = {
@@ -21,21 +27,27 @@ def send_telegram(msg):
         "text": msg
     }
     try:
-        requests.post(url, data=data, timeout=5)
-    except:
-        pass
+        res = requests.post(url, data=data, timeout=5)
+        print("Telegram status:", res.status_code)
+        print("Telegram response:", res.text)
+    except Exception as e:
+        print("Telegram error:", e)
 
 
 # ===== ROUTES =====
+
 @app.get("/")
 def home():
     return {"status": "running"}
 
 
+@app.get("/status")
+def status():
+    return last_data
+
+
 @app.post("/predict")
 def predict(data: dict):
-    global last_state
-
     water = data["water_level"]
     rain = data["rain_intensity"]
 
@@ -43,8 +55,16 @@ def predict(data: dict):
     features = np.array([[water, rain]])
     state = int(model.predict(features)[0])
 
+    # ===== SAVE LAST DATA =====
+    last_data["water"] = water
+    last_data["rain"] = rain
+    last_data["state"] = state
+
+    print("STATE:", state)
+
     # ===== TELEGRAM TRIGGER =====
-    if state >= 2 and state != last_state:
+    if state >= 2:
+        print("Sending Telegram message...")
         msg = f"""⚠ FLOOD ALERT
 
 State: {state}
@@ -52,7 +72,5 @@ Water Level: {water}
 Rain Intensity: {rain}
 """
         send_telegram(msg)
-
-    last_state = state
 
     return {"state": state}
